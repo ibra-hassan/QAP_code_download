631cc56614bb111ff86e4c73fd9f7e5e11e5935f
Ibrahim Hassan <hemo.a.h.96@gmail.com>
2020-12-16 15:45:41 +0300

    support for multi-platform

--- QAP.py
+++ QAP.py
@@ -1,10 +1,14 @@
 import os
 import time
+import platform
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+
+SEPARATOR = "\\" if platform.system() == "Windows" else "/"
 
 
 class QuickAdminPanel:
@@ -24,58 +28,48 @@ class QuickAdminPanel:
             ec.presence_of_element_located(
                 (By.XPATH, '/html/body/div[3]/div[1]/div/div/div/div/div[1]/div/div[1]/div')))
         app_nav.click()
-        self._sleep(2)
+        # self._sleep(2)
         menu = self.wait.until(
             ec.presence_of_element_located((By.XPATH, '/html/body/div[3]/div[1]/div/div/div/div/div[1]/div')))
         files = menu.find_elements_by_class_name('fs-file')
         folders = menu.find_elements_by_class_name('fs-dir-closed')
-        self.create_files(files_list=files, folder_name=None)
+        self.create_files(files_list=files,
+                          folder_name=self._get_project_name())
         self.create_folder(folder_list=folders)
         self._close()
 
-    def create_files(self, files_list, folder_name=None):
-        if folder_name is not None:
-            os.system(f'mkdir {folder_name}')
+    def create_files(self, files_list, folder_name):
+        os.system(f'mkdir {folder_name}')
         if len(files_list) > 0:
             for file in files_list:
-                time.sleep(2)
+                # time.sleep(2)
                 file.click()
-                file_name = self.driver.find_element_by_xpath(
-                    "/html/body/div[3]/div[1]/div/div/header/h5").text
-                file_code = self.driver.find_element_by_xpath(
-                    "/html/body/div[3]/div[1]/div/div/div/div/div[2]/pre/code").text
                 try:
-                    if folder_name is not None:
-                        new_file = open(f'{folder_name}\\{file_name}', 'bw')
-                        new_file.write(bytearray(file_code, encoding='utf8'))
-                        new_file.close()
-                    else:
-                        new_file = open(f'{file_name}', 'bw')
-                        new_file.write(bytearray(file_code, encoding='utf8'))
-                        new_file.close()
-                except UnicodeError:
-                    self.file_failed.append(f'{folder_name}\\{file_name}')
+                    file_name = self.driver.find_element_by_xpath(
+                        "/html/body/div[3]/div[1]/div/div/header/h5").text
+                    file_code = self.driver.find_element_by_xpath(
+                        "/html/body/div[3]/div[1]/div/div/div/div/div[2]/pre/code").text
+                except NoSuchElementException:
+                    file_code = ""
+                new_file = open(
+                    f'{folder_name}{SEPARATOR}{file_name}', 'bw')
+                new_file.write(bytearray(file_code, encoding='utf8'))
+                new_file.close()
 
     def create_folder(self, folder_list):
         if len(folder_list) > 0:
             for folder in folder_list:
                 if folder.text not in self.path:
                     self.path.append(folder.text)
-                time.sleep(2)
+                # time.sleep(2)
                 folder.click()
                 files = folder.find_element(
                     By.XPATH, '..').find_elements_by_class_name('fs-file')
-                self.create_files(files, '\\'.join(self.path))
+                self.create_files(files, SEPARATOR.join(self.path))
                 folders = folder.find_element(
                     By.XPATH, '..').find_elements_by_class_name('fs-dir-closed')
                 self.create_folder(folders)
                 self.path.pop(-1)
-                if len(self.path) == 1:
-                    print(
-                        "=====================================================================")
-                    print(f"{folder.text} ==> Done")
-                    print(
-                        "=====================================================================")
 
     def _log_in(self):
         self._load_page()
@@ -90,13 +84,19 @@ class QuickAdminPanel:
         self.wait.until(ec.presence_of_element_located(
             (By.TAG_NAME, 'button'))).click()
 
+    def _get_project_name(self):
+        project_name = self.wait.until(ec.presence_of_element_located(
+            (By.XPATH, '/html/body/div[1]/aside/a/span'))).text.strip()
+        self.path.append(project_name)
+        return project_name
+
     @staticmethod
     def _sleep(sec):
         time.sleep(sec)
 
     def _load_page(self):
         self.driver.get(self.url)
-        time.sleep(5)
+        # self._sleep(5)
 
     def _show_code(self):
         # Get View Code nav bar
@@ -112,8 +112,8 @@ class QuickAdminPanel:
 
 if __name__ == '__main__':
     QAP = QuickAdminPanel(
-        email='cedix84400@oramail.net',
-        password='cedix84400@oramail.netas',
-        url='https://2019.quickadminpanel.com/builder/30770/menu/index'
+        email='hadeca5572@hmnmw.com',
+        password='hadeca5572@hmnmw.comasas',
+        url='https://2019.quickadminpanel.com/builder/38583/menu/index'
     )
     QAP.get_code()
